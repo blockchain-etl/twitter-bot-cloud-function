681d7b123bf83a0c68f4b70348a4b8f688704eb5
medvedev1088 <medvedev1088@gmail.com>
2019-10-29 14:30:21 +0700

    Change message

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const request = require('request-promise');
 const Buffer = require('safe-buffer').Buffer;
 
 const NEW_DIRECT_MESSAGE_ENDPOINT = "https://api.twitter.com/1.1/direct_messages/events/new.json";
+const TYPE_ETHEREUM_ANOMALOUS_VALUE = "ethereum_anomalous_value";
+const TYPE_ETHEREUM_ANOMALOUS_GAS_COST = "ethereum_anomalous_gas_cost";
+const TYPE_BITCOIN_ANOMALOUS_VALUE = "bitcoin_anomalous_value";
 
 // subscribe is the main function called by Cloud Functions.
 module.exports.subscribe = async (data, context, callback) => {
@@ -14,10 +17,14 @@ module.exports.subscribe = async (data, context, callback) => {
     if (message) {
         const directMessageRecipientId = getEnvVar('DIRECT_MESSAGE_RECIPIENT_ID', '');
         if (directMessageRecipientId) {
+            console.log(`Sending direct message to Twitter ${message}.`);
             await directMessageInTwitter(message, parseInt(directMessageRecipientId));
         } else {
+            console.log(`Sending post to Twitter ${message}.`);
             await postInTwitter(message)
         }
+    } else {
+        console.log("message was null.")
     }
 
     callback();
@@ -32,14 +39,18 @@ const buildTwitterMessage = (parsedMessage) => {
 
     let message = null;
 
-    if (type === "anomalous_ether_value") {
+    if (type === TYPE_ETHEREUM_ANOMALOUS_VALUE) {
         const value = formatEtherValue(parsedMessage.transaction.value);
         message = `Transaction with an unusually high value of ${value} ETH: https://etherscan.io/tx/${parsedMessage.transaction.hash}. ` +
-            `Only one transaction had a higher value in the last 7 days.`
-    } else if (type === "anomalous_gas_cost") {
+            `Only one transaction had a comparable value in the last 7 days.`;
+    } else if (type === TYPE_ETHEREUM_ANOMALOUS_GAS_COST) {
         const gasCost = formatEtherValue(parsedMessage.gas_cost);
         message = `Transaction with an unusually high gas cost of ${gasCost} ETH: https://etherscan.io/tx/${parsedMessage.transaction.hash}. ` +
-            `Only one transaction had a higher value in the last 7 days.`
+            `Only one transaction had a comparable value in the last 7 days.`;
+    } else if (type === TYPE_BITCOIN_ANOMALOUS_VALUE) {
+        const value = formatBitcoinValue(parsedMessage.transaction.input_value);
+        message = `Transaction with an unusually high value of ${value} BTC: https://www.blockchain.com/btc/tx/${parsedMessage.transaction.hash}. ` +
+            `Only one transaction had a comparable value in the last 7 days.`;
     }
 
     return message;
@@ -50,15 +61,19 @@ const validateParsedMessage = (parsedMessage) => {
         console.log("type is undefined in parsedMessage");
         return false;
     }
-    if (parsedMessage.type === "anomalous_ether_value") {
+    if (parsedMessage.type === TYPE_ETHEREUM_ANOMALOUS_VALUE) {
         if (typeof parsedMessage.transaction === "undefined" || typeof parsedMessage.transaction.value === "undefined") {
             console.log("transaction.value is undefined in parsedMessage");
             return false;
         }
-    }
-    if (parsedMessage.type === "anomalous_gas_cost") {
+    } else if (parsedMessage.type === TYPE_ETHEREUM_ANOMALOUS_GAS_COST) {
         if (typeof parsedMessage.gas_cost === "undefined") {
             console.log("transaction.gas_cost is undefined in parsedMessage");
+            return false;
+        }
+    } else if (parsedMessage.type === TYPE_BITCOIN_ANOMALOUS_VALUE) {
+        if (typeof parsedMessage.transaction === "undefined" || typeof parsedMessage.transaction.input_value === "undefined") {
+            console.log("transaction.input_value is undefined in parsedMessage");
             return false;
         }
     }
@@ -108,6 +123,11 @@ const formatAmount = (num) => {
 
 const formatEtherValue = (value) => {
     const adjustedValue = value / Math.pow(10, 18);
+    return formatAmount(adjustedValue);
+};
+
+const formatBitcoinValue = (value) => {
+    const adjustedValue = value / Math.pow(10, 8);
     return formatAmount(adjustedValue);
 };
 
